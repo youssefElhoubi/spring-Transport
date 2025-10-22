9baeb74508bd9edd7873d94ce689de61c5946754
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-10-22 11:33:27 +0100

    feat: filed the method of the DeliveryService

--- src/main/java/com/transports/transport/service/DeliveryService.java
+++ src/main/java/com/transports/transport/service/DeliveryService.java
@@ -1,11 +1,38 @@
 package com.transports.transport.service;
 
+import com.transports.transport.entities.Delivery;
 import com.transports.transport.repository.DeliveryRepository;
 
+import java.util.List;
+
 public class DeliveryService {
-    private DeliveryRepository deliveryService;
+    private DeliveryRepository deliveryRepository;
 
     public DeliveryService(DeliveryRepository deliveryService) {
-        this.deliveryService = deliveryService;
+        this.deliveryRepository = deliveryService;
+    }
+
+    public List<Delivery> findAll() {
+        return deliveryRepository.findAll();
+    }
+    public Delivery findById(Long id) {
+        return deliveryRepository.findById(id).get();
+    }
+    public Delivery save (Delivery d){
+        return deliveryRepository.save(d);
+    }
+    public Delivery update (Delivery d){
+        Delivery delivery = deliveryRepository.findById(d.getId()).orElseThrow(()-> new RuntimeException("Delivery not found with ID: " + d.getId()));
+
+        delivery.setAddress(d.getAddress());
+        delivery.setLatitude(d.getLatitude());
+        delivery.setLongitude(d.getLongitude());
+        delivery.setWeight(d.getWeight());
+        delivery.setVolume(d.getVolume());
+        delivery.setPreferredTimeSlot(d.getPreferredTimeSlot());
+        delivery.setStatus(d.getStatus());
+        delivery.setSequenceOrder(d.getSequenceOrder());
+        delivery.setTour(d.getTour()); // if you want to allow updating the tour
+        return deliveryRepository.save(delivery);
     }
 }
