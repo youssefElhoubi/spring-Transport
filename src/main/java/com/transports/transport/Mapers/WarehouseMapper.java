b8f39f02a9399fae56f32d2f4d896621b8997528
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-10-22 14:09:41 +0100

    added a maper for the Warehouse

---
+++ src/main/java/com/transports/transport/Mapers/WarehouseMapper.java
@@ -0,0 +1,16 @@
+package com.transports.transport.Mapers;
+
+import com.transports.transport.DTOS.VehiculDTO;
+import com.transports.transport.DTOS.WarehouseDTO;
+import com.transports.transport.entities.Vehicul;
+import com.transports.transport.entities.Warehouse;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface WarehouseMapper {
+    WarehouseMapper INSTENCE = Mappers.getMapper(WarehouseMapper.class);
+
+    Warehouse toEntity(WarehouseDTO dto);
+    WarehouseDTO toDTO (Warehouse warehouse );
+}
