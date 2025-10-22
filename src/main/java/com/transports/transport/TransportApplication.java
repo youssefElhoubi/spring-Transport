97d817f36e0f3fba634b7be269abc89a81c04a4e
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-10-22 15:29:05 +0100

    adde bean if the deliver service in the cotroller and fixed a problim in main

--- src/main/java/com/transports/transport/TransportApplication.java
+++ src/main/java/com/transports/transport/TransportApplication.java
@@ -2,8 +2,10 @@ package com.transports.transport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
 public class TransportApplication {
 
     public static void main(String[] args) {SpringApplication.run(TransportApplication.class, args);}
--- src/main/java/com/transports/transport/controllers/DeliveryController.java
+++ src/main/java/com/transports/transport/controllers/DeliveryController.java
@@ -1,15 +1,17 @@
 package com.transports.transport.controllers;
 
+import com.transports.transport.service.DeliveryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/deliveries")
 public class DeliveryController {
-    private final DeliveryController deliveryController;
-
-    public DeliveryController(DeliveryController deliveryController) {
-        this.deliveryController = deliveryController;
+    private final DeliveryService deliverySer;
+    @Autowired
+    public DeliveryController(DeliveryService deliveryService) {
+        this.deliverySer = deliveryService;
     }
 
 }
