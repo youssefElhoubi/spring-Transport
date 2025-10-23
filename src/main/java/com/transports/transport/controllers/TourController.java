24c75b487d24451bf0f2a4e820ebe0b3395a8b4c
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-10-23 10:50:18 +0100

    Refactor TourController to use TourService instead of TourRepository

--- src/main/java/com/transports/transport/controllers/TourController.java
+++ src/main/java/com/transports/transport/controllers/TourController.java
@@ -1,14 +1,29 @@
 package com.transports.transport.controllers;
 
 import com.transports.transport.MapperImplementation.TourMapperImpl;
+import com.transports.transport.entities.Delivery;
+import com.transports.transport.entities.Tour;
 import com.transports.transport.repository.tourRepository;
+import com.transports.transport.service.TourService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/tour")
 public class TourController {
-    private final tourRepository tourRepository;
+    private final TourService tourService;
     private final TourMapperImpl tourMapper;
-    public TourController(tourRepository tourRepository, TourMapperImpl tourMapper) {
-        this.tourRepository=tourRepository;
+    public TourController(TourService tourService, TourMapperImpl tourMapper) {
+        this.tourService=tourService;
         this.tourMapper =tourMapper;
     }
+    @GetMapping("/all")
+    public List<Tour> all() {
+        return tourService.findAll();
+    }
+
 
 }
