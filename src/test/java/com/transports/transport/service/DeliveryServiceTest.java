44f603f31b827ee9659ee54a83dfd1fcf753e3d9
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-10-24 14:48:21 +0100

    Added test case for findById method when no delivery is found

--- src/test/java/com/transports/transport/service/DeliveryServiceTest.java
+++ src/test/java/com/transports/transport/service/DeliveryServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.beans.Expression;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
@@ -29,5 +30,14 @@ class DeliveryServiceTest {
         Delivery result = deliveryService.findById(1L);
         assertNotNull(result);
         assertEquals(1L, result.getId());
+    }
+    @Test
+    void testFindById_notFount(){
+        Delivery delivery = new Delivery();
+        when(deliveryRepository.findById(1L)).thenReturn(Optional.empty());
+        Exception expression = assertThrows( RuntimeException.class,()->{
+            deliveryService.findById(1l);
+        });
+        assertTrue(expression.getMessage().contains("no Delivery found with this ID"));
     }
 }
