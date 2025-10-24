e89f069db256c094f3e687900db4bb341ae34ea2
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-10-24 10:50:21 +0100

    Added Swagger annotations to DeliveryController for API documentation

--- src/main/java/com/transports/transport/controllers/DeliveryController.java
+++ src/main/java/com/transports/transport/controllers/DeliveryController.java
@@ -5,6 +5,9 @@ import com.transports.transport.Mapers.DeliveryMaper;
 import com.transports.transport.MapperImplementation.DeliveryMapperImpl;
 import com.transports.transport.entities.Delivery;
 import com.transports.transport.service.DeliveryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +26,11 @@ public class DeliveryController {
         this.deliveryMaper = deliveryMaper;
     }
 
+    @Operation(summary = "method allows to get all the Deliveries ", description = "make a request for the end point and get all the existing Deliveries")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200",description = "all the Deliveries "),
+            @ApiResponse(responseCode = "400", description = "Invalid input data")
+    })
     @GetMapping("/all")
     public List<Delivery> all() {
         return deliverySer.findAll();
@@ -46,13 +54,14 @@ public class DeliveryController {
         delivery = deliverySer.update(delivery);
         return ResponseEntity.ok(delivery);
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id){
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         Delivery delivery = deliverySer.findById(id);
         if (delivery == null) {
             return ResponseEntity.notFound().build();
         }
         deliverySer.Delete(id);
-        return  ResponseEntity.ok("delivery deleted ");
+        return ResponseEntity.ok("delivery deleted ");
     }
 }
